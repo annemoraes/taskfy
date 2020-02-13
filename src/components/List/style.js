62384e7441b38181a10774a0583bd58cfd20f069
Anne <ilarymoraes@hotmail.com>
2020-02-13 15:30:31 -0300

    drag and drop adjustments

--- src/components/List/style.js
+++ src/components/List/style.js
@@ -5,19 +5,23 @@ export const Container = styled.div`
   height: 100%;
   flex: 0 0 320px;
   opacity: ${props => props.done ? 0.6 : 1};
+
   & + div {
     border-left: 1px solid rgba(0, 0, 0, 0.05);
   }
+
   header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     height: 42px;
+
     h2 {
       font-weight: 500;
       font-size: 16px;
       padding: 0 10px;
     }
+
     button {
       width: 42px;
       height: 42px;
@@ -27,6 +31,7 @@ export const Container = styled.div`
       cursor: pointer;
     }
   }
+
   ul {
     margin-top: 30px;
   }
