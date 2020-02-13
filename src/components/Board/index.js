71d2d931f4e9f34add32fbdaa88285bd9a53afc8
Anne <ilarymoraes@hotmail.com>
2020-02-13 01:30:29 -0300

    implementing drag and drop

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import List from '../List';
+import produce from 'immer';
+import BoardContext from './context';
 import { loadLists } from '../../services/api';
 import { Container } from './style';
 
-const lists = loadLists();
+const data = loadLists();
 
-export default function Board(){
-    return(
-        <Container>
-            {lists.map(list => <List key={list.title} data={list}/>)}
-        </Container>
-    );
+export default function Board() {
+  const [lists, setLists] = useState(data);
+
+  function move(fromList, toList, from, to) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromList].cards[from];
+
+      draft[fromList].cards.splice(from, 1);
+      draft[toList].cards.splice(to, 0, dragged);
+    }))
+  }
+
+  return (
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => <List key={list.title} index={index} data={list} />)}
+      </Container>
+    </BoardContext.Provider>
+  );
 }
